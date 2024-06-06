af712f3e6f04d29ce947689fa9182224abc3f8d6
Nathan Rydel <nathanrydel@gmail.com>
2024-06-05 21:19:49 -0700

    add CRUD functionality
    - GET /api/todos to list all Todo items
    - POST /api/todos to create a new Todo item
    - GET /api/todos/:id to get a specific Todo item
    - PATCH /api/todos/:id to update a specific Todo item
    - DELETE /api/todos/:id to delete a specific Todo item

--- main.go
+++ main.go
@@ -3,14 +3,120 @@ package main
 import (
 	"log"
 
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v2"
 )
+
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
+type UpdateTodo struct {
+	Completed *bool   `json:"completed"`
+	Body      *string `json:"body"`
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
+	todos := []Todo{}
+
+	// Return the home page
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	// Return an array of all todos
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	// Create and return a new todo
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // {id: 0, completed: false, body: ""}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	// Return a specific todo
+	app.Get("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+
+		for _, todo := range todos {
+			if todo.ID == id {
+				return c.Status(200).JSON(todo)
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	// Update a specific todo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+
+		var updateData UpdateTodo
+
+		if err := c.BodyParser(&updateData); err != nil {
+			return err
+		}
+
+		for i, todo := range todos {
+			if todo.ID == id {
+				if updateData.Completed != nil {
+					todos[i].Completed = *updateData.Completed
+				}
+
+				if updateData.Body != nil {
+					if *updateData.Body == "" {
+						return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+					}
+					todos[i].Body = *updateData.Body
+				}
+
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	// Delete a specific todo
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+
+		for i, todo := range todos {
+			if todo.ID == id {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"message": "Todo deleted"})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
 	})
 
 	log.Fatal(app.Listen(":8080"))
