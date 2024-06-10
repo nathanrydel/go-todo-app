23cfab76eb1d5252420f2d36bca694640ddcf7f2
Nathan Rydel <nathanrydel@gmail.com>
2024-06-09 18:13:42 -0700

    frontend client initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Container, Stack } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <Stack h='100vh'>
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
