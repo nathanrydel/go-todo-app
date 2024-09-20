bf5a8af2e4481525df343f3593cacdf9151fc077
Nathan Rydel <nathanrydel@gmail.com>
2024-09-19 21:54:12 -0700

    create a theme switcher hook
    - using useEffect to handle side effects
    - using useState for tracking
    - storing choice in localStorage

---
+++ client/src/hooks/useTheme.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export const useTheme = () => {
+  const [theme, setTheme] = useState<string>(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('theme') || 'light';
+    }
+    return 'light';
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === 'dark') {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  return { theme, toggleTheme };
+};
