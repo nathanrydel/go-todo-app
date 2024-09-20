a5739843d0dff83162f8fc2ddd8d3c37a5f74c83
Nathan Rydel <nathanrydel@gmail.com>
2024-09-20 12:02:56 -0700

    Update header responsiveness

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,26 +1,28 @@
-import React from "react"
-import { Button } from "@/components/ui/button"
-import { CardHeader, CardTitle } from "@/components/ui/card"
-import { SunMoon } from "lucide-react"
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { SunMoon } from "lucide-react";
 
 interface HeaderProps {
-  toggleTheme: () => void
+  toggleTheme: () => void;
 }
 
 export const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
   return (
-    <CardHeader className="flex justify-between items-center">
-      <div className="flex items-center space-x-2">
-      <img src="react.png" alt="React Logo" className="h-12" />
-        <span className="text-4xl">+</span>
-        <img src="gopher.png" alt="Golang Gopher Mascot" className="h-8" />
-        <span className="text-2xl">=</span>
-        <span className="text-2xl">🦁</span>
+    <header className="w-full flex justify-center mb-8">
+      <div className="w-full lg:w-3/4 flex justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <img src="react.png" alt="React Logo" className="h-16 ms-4" />
+          <span className="text-4xl mx-2">+</span>
+          <img src="gopher.png" alt="Golang Gopher Mascot" className="h-16" />
+        </div>
+        <div className="flex items-center space-x-4">
+          <h2 className="text-xl font-normal">Daily Tasks</h2>
+          <Button variant="ghost" size="icon" onClick={toggleTheme}>
+            <SunMoon className="h-5 w-5" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </div>
       </div>
-      <CardTitle className="text-xl font-normal">Daily Tasks</CardTitle>
-      <Button variant="ghost" size="icon" onClick={toggleTheme}>
-        <SunMoon className="h-5 w-5" />
-      </Button>
-    </CardHeader>
-  )
-}
+    </header>
+  );
+};
